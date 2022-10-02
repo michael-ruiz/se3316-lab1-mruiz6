214424f11f8d7219a82adc494a421635129ce799
Michael Ruiz <mruiz6@uwo.ca>
2022-10-02 00:38:27 -0400

    made the result box updates dynamically

--- index.js
+++ index.js
@@ -1,26 +1,4 @@
-var description = 
-[
-    "Bulbasaur \n#001 Type: Grass Height: 2' 04\" Weight: 15.2 lbs",
-    "Ivysaur \n#002 Type: Grass Height: 3' 03\" Weight: 28.7 lbs",
-    "Venusaur \n#003 Type: Grass Height: 6' 07\" Weight: 220.5 lbs",
-    "Charmander \n#004 Type: Fire Height: 2' 00\" Weight: 18.7 lbs",
-    "Charmeleon \n#005 Type: Fire Height: 3' 07\" Weight: 41.9 lbs",
-    "Charizard \n#006 Type: Fire Height: 5' 07\" Weight: 199.5 lbs",
-    "Squirtle \n#007 Type: Water Height: 1' 08\" Weight: 19.8 lbs",
-    "Wartortle \n#008 Type: Water Height: 3' 03\" Weight: 49.6 lbs",
-    "Blastoise \n#009 Type: Water Height: 5' 03\" Weight: 188.5 lbs",
-    "Caterpie \n#010 Type: Bug Height: 1' 00\" Weight: 6.4 lbs",
-    "Metapod \n#011 Type: Bug Height: 2' 04\" Weight: 21.8 lbs",
-    "Butterfree \n#012 Type: Bug Height: 3' 07\" Weight: 70.5 lbs",
-    "Weedle \n#013 Type: Bug Height: 1' 00\" Weight: 7.1 lbs",
-    "Kakuna \n#014 Type: Bug Height: 2' 00\" Weight: 22.0 lbs",
-    "Beedrill \n#015 Type: Bug Height: 3' 03\" Weight: 65.0 lbs",
-    "Pidgey \n#016 Type: Normal Height: 1' 00\" Weight: 4.0 lbs",
-    "Pidgeotto \n#017 Type: Normal Height: 3' 07\" Height: 66.1 lbs",
-    "Pidgeot \n#018 Type: Normal Height: 4' 11\" Weight: 87.1 lbs",
-    "Rattata \n#019 Type: Normal Height: 1' 00\" Weight: 7.7 lbs",
-    "Raticate \n#020 Type: Normal Height: 2' 04\" Weight: 40.8 lbs"
-]
+var firstRun = true;
 
 function allLetter()
 {
@@ -77,8 +55,9 @@ function searchName()
     let input, pokemon
     let result = "";
     let count = 0;
+    let resultList = document.createElement("ul");
     input = document.getElementById("searchName");
-    pokemon = document.getElementsByTagName("li");
+    pokemon = document.getElementsByClassName("static");
 
     for (i = 0; i < pokemon.length; i++)
     {
@@ -89,14 +68,22 @@ function searchName()
         if (pName.includes(text) && count < 5)
         {
             text = pokemon[i].innerHTML;
-            result += `<li>${text}</li>`;
+            result += `<li class="result">${text}</li>`;
             count++;
         }
     }
 
+    if (document.getElementById("resultList") != null)
+    {
+        document.getElementById("resultList").parentNode.removeChild(document.getElementById("resultList"));
+    }
+
     if (result != "")
     {
-       document.getElementById("results").innerHTML = result;
+       resultList.innerHTML = result;
+       resultList.className = "list";
+       resultList.id = "resultList";
+       document.getElementById("results").appendChild(resultList);
        result = "";
     }
 }
